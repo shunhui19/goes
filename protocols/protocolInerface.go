022593e7f2e6516f331c60e746267e32163a4a65
frank <shunhui29@163.com>
2019-12-16 21:48:12 +0800

    add comment to protocolInterface

--- protocols/protocolInerface.go
+++ protocols/protocolInerface.go
@@ -1,10 +1,12 @@
+// Protocol the interface of protocol, include three method,
+// you can define custom protocol through implement three method.
 package protocols
 
 type Protocol interface {
-	// Encode encode package before send to client
+	// Encode encode package before send to client.
 	Encode()
-	// Decode decode package and emit
+	// Decode decode package and emit.
 	Decode()
-	// Input check the integrity of package
+	// Input check the integrity of package.
 	Input() int
 }

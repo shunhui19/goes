b32542b45946b29e152a4bf85fa9196b909def23
frank <shunhui29@163.com>
2020-01-07 15:20:24 +0800

    undone

--- example/client.go
+++ example/client.go
@@ -12,31 +12,34 @@ import (
 
 func main() {
 	var w sync.WaitGroup
-	for i := 0; i < 500; i++ {
+	clientCount := 50
+	sendCount := 5
+	for i := 0; i < clientCount; i++ {
 		w.Add(1)
-		go func() {
+		go func(i int) {
 			defer w.Done()
 			conn, err := net.Dial("tcp", "127.0.0.1:8080")
 			if err != nil {
 				log.Fatal(err)
 			}
+			defer conn.Close()
 
 			// receive
-			go func() {
-				buf := make([]byte, 1024)
-				for {
-					n, err := conn.Read(buf)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
-					//fmt.Println(bytes.Contains(buf[:n], []byte("\n")))
-					fmt.Println(string(buf[:n]))
-				}
-			}()
+			//go func() {
+			//	buf := make([]byte, 1024)
+			//	for {
+			//		n, err := conn.Read(buf)
+			//		if err != nil {
+			//			log.Fatal(err.Error())
+			//		}
+			//		//fmt.Println(bytes.Contains(buf[:n], []byte("\n")))
+			//		fmt.Println(string(buf[:n]))
+			//	}
+			//}()
 
 			rand.Seed(time.Now().Unix())
-			for i := 0; i < 10; i++ {
-				n, err := conn.Write([]byte("hello" + strconv.Itoa(i) + "\n"))
+			for j := 0; j < sendCount; j++ {
+				n, err := conn.Write([]byte(fmt.Sprintf("[goroutine-%d]", i) + "hello" + strconv.Itoa(j) + "\n"))
 				if err != nil {
 					log.Fatal(err)
 				}
@@ -44,8 +47,9 @@ func main() {
 				//time.Sleep(time.Duration(rand.Int31n(5)) * time.Second)
 			}
 
-			//time.Sleep(10 * time.Second)
-		}()
+			// 保持客户端收到服务端数据之前不关闭连接
+			//time.Sleep(5 * time.Second)
+		}(i)
 	}
 
 	w.Wait()
--- example/main.go
+++ example/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"goes"
 	"goes/connections"
+	"goes/lib"
 	"goes/protocols"
 )
 
 func main() {
-	goer := goes.NewGoer("127.0.0.1:8080", protocols.NewTextProtocol(), "udp")
+	goer := goes.NewGoer("127.0.0.1:8080", protocols.NewTextProtocol(), "tcp")
 	//goer.OnConnect = func(connection *connections.TcpConnection) {
 	//	fmt.Printf("remoteAddress: %s\n", connection.GetRemoteAddress())
 	//	fmt.Printf("remoteIp: %s\n", connection.GetRemoteIp())
@@ -20,17 +21,18 @@ func main() {
 	//}
 
 	goer.OnConnect = func(connection connections.ConnectionInterface) {
-		connection.Send("hello, world", false)
+		//connection.Send("hello, world", false)
 	}
 
 	goer.OnMessage = func(connection connections.ConnectionInterface, data []byte) {
-		fmt.Println(string(data))
-		connection.Send(fmt.Sprintf("the client send message is %v", string(data)), false)
+		fmt.Println("Receive:", len(string(data)))
+		//connection.Send(string(data), false)
+		//connection.Send(fmt.Sprintf("the client send message is %v", string(data)), false)
 		//fmt.Println(status)
 	}
 
 	goer.OnClose = func() {
-		fmt.Println("closed")
+		lib.Info("client is closed")
 	}
 
 	//goer.Transport = "udp"

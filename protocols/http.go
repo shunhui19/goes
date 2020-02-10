2a72b371f8ca8b30e4aa444abec60268df39bbf6
frank <shunhui29@163.com>
2020-02-10 17:01:37 +0800

    http-undone

--- protocols/http.go
+++ protocols/http.go
@@ -1,2 +1,269 @@
-// http protocol
+// http protocol.
 package protocols
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"goes/lib"
+	"reflect"
+	"regexp"
+	"strconv"
+	"time"
+)
+
+// methods the method of http request.
+var methods = [...]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}
+
+// httpCode the http code of response.
+var httpCode = map[int]string{
+	100: "Continue",
+	101: "Switching Protocols",
+	200: "OK",
+	201: "Created",
+	202: "Accepted",
+	203: "Non-Authoritative Information",
+	204: "No Content",
+	205: "Reset Content",
+	206: "Partial Content",
+	300: "Multiple Choices",
+	301: "Moved Permanently",
+	302: "Found",
+	303: "See Other",
+	304: "Not Modified",
+	305: "Use Proxy",
+	306: "(Unused)",
+	307: "Temporary Redirect",
+	400: "Bad Request",
+	401: "Unauthorized",
+	402: "Payment Required",
+	403: "Forbidden",
+	404: "Not Found",
+	405: "Method Not Allowed",
+	406: "Not Acceptable",
+	407: "Proxy Authentication Required",
+	408: "Request Timeout",
+	409: "Conflict",
+	410: "Gone",
+	411: "Length Required",
+	412: "Precondition Failed",
+	413: "Request Entity Too Large",
+	414: "Request-URL Too Long",
+	415: "Unsupported Media Type",
+	416: "Requested Range Not Satisfiable",
+	417: "Expectation Failed",
+	422: "Unprocessable Entity",
+	423: "Locked",
+	500: "Internal Server Error",
+	501: "Not Implemented",
+	502: "Bad Gateway",
+	503: "Service Unavailable",
+	504: "Gateway Timeout",
+	505: "Http Version Not Supported",
+}
+
+// Http struct of http.
+type Http struct {
+	instance *Http
+	// header header key-value.
+	header               map[string]interface{}
+	gzip                 bool
+	sessionPath          string
+	sessionName          string
+	sessionGcProbability int
+	sessionGcMaxLifeTime int
+	sessionStarted       bool
+	// sessionFile session file.
+	sessionFile string
+	// post post data.
+	post map[string]interface{}
+}
+
+// Input check the integrity of the package.
+func (h *Http) Input(recvBuffer []byte) interface{} {
+	position := bytes.IndexAny(recvBuffer, "\r\n\r\n")
+	if position == -1 {
+		if len(recvBuffer) >= MaxPackageSize {
+			return false
+		}
+		return 0
+	}
+
+	header := bytes.SplitN(recvBuffer, []byte("\r\n\r\n"), 2)[0]
+	method := string(bytes.Split(header, []byte(" "))[0])
+
+	for _, v := range methods {
+		if v == method {
+			return h.getRequestSize(header, method)
+		}
+	}
+	lib.Warn("HTTP/1.1 400 Bad Request\r\n\r\n")
+	return 0
+	//return 0, "HTTP/1.1 400 Bad Request\r\n\r\n"
+}
+
+// Encode http encode.
+func (h *Http) Encode(data []byte) []byte {
+	// default http-code.
+	var header string
+	if _, ok := h.header["Http-Code"]; !ok {
+		header = "HTTP/1.1 200 OK\r\n"
+	} else {
+		header = h.header["Http-Code"].(string) + "\r\n"
+		delete(h.header, "Http-Code")
+	}
+
+	// content-type.
+	if _, ok := h.header["Content-Type"]; !ok {
+		header += "Content-Type: text/html;charset=utf-8\r\n"
+	}
+
+	// other headers key-value.
+	for k, v := range h.header {
+		if k == "Set-Cookie" && reflect.TypeOf(v).Kind() == reflect.Map {
+
+		} else {
+			header += v.(string) + "\r\n"
+		}
+	}
+	if h.gzip {
+		header += "Content-Encoding: gzip\r\n"
+		var buf bytes.Buffer
+		zw := gzip.NewWriter(&buf)
+		_, err := zw.Write(data)
+		if err != nil {
+			lib.Error("gzip content error: %s", err)
+		}
+		if err := zw.Close(); err != nil {
+			lib.Warn(err.Error())
+		}
+		data = buf.Bytes()
+	}
+
+	// header.
+	header += "Server: Goes/" + "\r\nContent-Length: " + strconv.Itoa(len(data)) + "\r\n\r\n"
+
+	// save session.
+
+	// the whole http package.
+	return append([]byte(header), data...)
+}
+
+// Decode parse POST, GET, COOKIE.
+func (h *Http) Decode(recvBuffer []byte) []byte {
+	if h.header == nil {
+		h.header = make(map[string]interface{})
+	}
+	h.header["Connection"] = "Connection: keep-alive"
+	h.instance = &Http{}
+	server := map[string]interface{}{
+		"QUERY_STRING":         "",
+		"REQUEST_METHOD":       "",
+		"REQUEST_URL":          "",
+		"SERVER_PROTOCOL":      "",
+		"SERVER_SOFTWARE":      "Goes",
+		"SERVER_NAME":          "",
+		"HTTP_HOST":            "",
+		"HTTP_USER_AGENT":      "",
+		"HTTP_ACCEPT":          "",
+		"HTTP_ACCEPT_LANGUAGE": "",
+		"HTTP_ACCEPT_ENCODING": "",
+		"HTTP_COOKIE":          "",
+		"HTTP_CONNECTION":      "",
+		"CONTENT_TYPE":         "",
+		"REMOTE_ADDR":          "",
+		"REMOTE_PORT":          "",
+		"REQUEST_TIME":         time.UTC,
+	}
+
+	// parse header.
+	packages := bytes.SplitN(recvBuffer, []byte("\r\n\r\n"), 2)
+	header, body := packages[0], packages[1]
+	headerData := bytes.Split(header, []byte("\r\n"))
+	headerData2 := bytes.Split(headerData[0], []byte(" "))
+	server["REQUEST_METHOD"], server["REQUEST_URL"], server["REQUEST_PROTOCOL"] = string(headerData2[0]), string(headerData2[1]), string(headerData2[2])
+
+	// parse general header.
+	//httpPostBoundary := ""
+	for i, v := range headerData {
+		if i == 0 || len(v) == 0 {
+			continue
+		}
+		keyValue := bytes.SplitN(v, []byte(":"), 2)
+		key := string(bytes.ToUpper(bytes.Replace(keyValue[0], []byte("-"), []byte("_"), 1)))
+		value := bytes.Trim(keyValue[1], " ")
+		server["HTTP_"+key] = value
+		switch key {
+		// HTTP_HOST.
+		case "HOST":
+			tmp := bytes.Split(value, []byte(":"))
+			server["SERVER_NAME"] = string(tmp[0])
+			if len(tmp) > 1 {
+				server["SERVER_PORT"] = string(tmp[1])
+			}
+		// cookie.
+		case "COOKIE":
+			server["HTTP_COOKIE"] = string(bytes.Replace(value, []byte("; "), []byte("&"), -1))
+		// content-type.
+		case "CONTENT_TYPE":
+			re := regexp.MustCompile("boundary=\\S+")
+			matches := re.FindSubmatch(v)
+			if matches == nil {
+				if pos := bytes.IndexAny(v, ";"); pos != -1 {
+					server["CONTENT_TYPE"] = v[:pos]
+				} else {
+					server["CONTENT_TYPE"] = string(v)
+				}
+			} else {
+				server["CONTENT_TYPE"] = "multipart/form-data"
+				//httpPostBoundary = "--" + string(matches[1])
+			}
+		// content-length.
+		case "CONTENT_LENGTH":
+			server["CONTENT_LENGTH"] = string(value)
+		// upgrade.
+		case "upgrade":
+		default:
+		}
+	}
+	if v, ok := server["HTTP_ACCEPT_ENCODING"]; ok && v == "gzip" {
+		h.gzip = true
+	}
+
+	// parse post
+	if server["REQUEST_METHOD"] == "POST" {
+		if server["CONTENT_TYPE"] != "" {
+			switch server["CONTENT_TYPE"] {
+			case "multipart/form-data":
+			case "application/json":
+				err := json.Unmarshal(body, &h.post)
+				if err != nil {
+					lib.Warn("decode data of post error: %v", err)
+				}
+			case "application/x-www-form-urlencoded":
+			}
+		}
+	}
+
+	fmt.Println(server)
+	return []byte{1}
+}
+
+// getRequestSize get whole size of the request.
+// includes the request headers and request body.
+func (h *Http) getRequestSize(header []byte, method string) int {
+	if method == "GET" || method == "OPTIONS" || method == "HEAD" || method == "DELETE" {
+		return len(header) + 4
+	}
+
+	re := regexp.MustCompile("Content-Length: \\d+")
+	matches := re.FindSubmatch(header)
+	if matches != nil {
+		contentLength := bytes.Trim(bytes.Split(matches[0], []byte(":"))[1], " ")
+		v, _ := strconv.ParseInt(string(contentLength), 10, 32)
+		return int(v) + 4
+	}
+
+	return 0
+}
---
+++ protocols/http_test.go
@@ -0,0 +1,48 @@
+package protocols
+
+import "testing"
+
+func TestHttp_getRequestSize(t *testing.T) {
+	type args struct {
+		header []byte
+		method string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "GET", args: args{header: []byte("GET / HTTP/1.1\r\nHost: www.frank.com\r\nConnection: keep-live\r\n"), method: "GET"}, want: 64},
+		{name: "OPTIONS", args: args{header: []byte("OPTIONS / HTTP/1.1\r\nHost: www.frank.com\r\nConnection: keep-live\r\n"), method: "OPTIONS"}, want: 68},
+		{name: "HEAD", args: args{header: []byte("HEAD / HTTP/1.1\r\nHost: www.frank.com\r\nConnection: keep-live\r\n"), method: "HEAD"}, want: 65},
+		{name: "DELETE", args: args{header: []byte("DELETE / HTTP/1.1\r\nHost: www.frank.com\r\nConnection: keep-live\r\n"), method: "DELETE"}, want: 67},
+		{name: "POST", args: args{header: []byte("POST / HTTP/1.1\r\nHost: www.frank.com\r\nContent-Length: 15\r\nConnection: keep-live\r\n"), method: "POST"}, want: 19},
+		{name: "PUT", args: args{header: []byte("PUT / HTTP/1.1\r\nHost: www.frank.com\r\nContent-Length: 15\r\nConnection: keep-live\r\n"), method: "PUT"}, want: 19},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := &Http{}
+			if got := h.getRequestSize(tt.args.header, tt.args.method); got != tt.want {
+				t.Errorf("getRequestSize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestHttp_Decode(t *testing.T) {
+	type args struct {
+		packet []byte
+	}
+	tests := []struct {
+		args
+	}{
+		{args{packet: []byte("GET /helloWorld HTTP/1.1\r\nHOST:www.frank.com\r\nCookie: BAIDUID=C88B6F1243FCB8D658349758E3340CB8:FG=1; BIDUPSID=C88B6F1243FCB8D658349758E3340CB8; PSTM=1556635667\r\n\r\n")}},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			h := &Http{}
+			h.Decode(tt.args.packet)
+		})
+	}
+}

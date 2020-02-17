f711d04c243cd5edecbbb728cb40362dd845fecb
frank <shunhui29@163.com>
2020-02-17 23:26:10 +0800

    client test

---
+++ example/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"goes/lib"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Fatal("connect server error: ", err)
+	}
+	defer conn.Close()
+
+	go func() {
+		for {
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil || err == io.EOF {
+				lib.Fatal("client read error: %v", err)
+			}
+			lib.Info("Received content: %v", string(buf[:n]))
+		}
+	}()
+
+	content := "hello, world\n"
+	_, err = conn.Write([]byte(content))
+	if err != nil {
+		lib.Warn("client send error: %v", err)
+	}
+	lib.Info("client send content: %v", content)
+
+	select {}
+}
